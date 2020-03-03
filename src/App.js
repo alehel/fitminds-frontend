0d828a510853929cccd851eee154ae79e42b99e6
EJL <eijle@eg.no>
2020-03-03 16:10:40 +0100

    can I do this?

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload if you want to live
-          forever!.
+          forever! And i sorely do!
         </p>
         <a
           className="App-link"
