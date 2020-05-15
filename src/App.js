b772cb28fa75ca6687cd748ba842bf33f7f6eda2
Exhalingdeath <eiliv.laegreid@gmail.com>
2020-05-15 09:49:56 +0200

    Revert "Feature/routing"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import LandingPage from './cmp/LandingPage';
 
 function App() {
+  const [stravaCode, setStravaCode] = useState(undefined);
+  const [accessDenied, setAccessDenied] = useState(false);
+
+  useEffect(() => {
+    const error = new URLSearchParams(window.location.search).get('error');
+
+    if(error) {
+      setAccessDenied(true);
+    } else {
+      const code = new URLSearchParams(window.location.search).get('code');
+      if(code !== null) {
+        setStravaCode(code);
+      }
+    }
+  }, [stravaCode, accessDenied]);
+
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path='/' exact component={LandingPage}/>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>
+        fitminds.run
+      </h1>
+
+      {accessDenied && 
+        <p>Failed to authenticate with strava</p>
+      }
+      
+      {stravaCode !== undefined 
+        ? <div>Your Strava Code is: {stravaCode}</div>
+        : <a
+            className="App-link"
+            href="https://www.strava.com/oauth/authorize?client_id=44056&redirect_uri=http://localhost:3000&response_type=code&scope=read"
+            rel="noopener noreferrer">
+            Log in with Strava
+          </a>
+      }
+    </div>
   );
 }
 
